ba400625f5c5c527c7e12367c4692f64a7132a29
samer <sameroayyoub@gmail.com>
2024-11-28 09:23:18 +0100

    first commit

---
+++ medev-TP3/src/main/java/fr/centrale/medev/tp3/MedevTP3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.centrale.medev.tp3;
+
+/**
+ *
+ * @author samer
+ */
+public class MedevTP3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
